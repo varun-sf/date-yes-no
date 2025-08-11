d1cec5ff06925657e7109a6baf5d20b10b1c3b0c
varun-sf <43672066+varun-sf@users.noreply.github.com>
2025-08-11 21:56:26 +0530

    Update Main.js
    temp

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,7 +8,7 @@ const Main = (props)=> {
     const [buttonSize, setButtonSize] = useState(1);
     const [index,setIndex] = useState(0);
    
-    const arr=["Why, No?","Press Yes na","Press yes","Plz Mam","You will miss a great day","You will miss a great day!!!","Pleaseeeeeee","Pleaseeeeeee","Enough madam you want to explode the screen?"]
+    // const arr=[""]
     let nose =()=>{
       setButtonSize(buttonSize+1);
       if(index>8){return;}
@@ -24,8 +24,8 @@ const Main = (props)=> {
   }
     return (
         <>
-        <h1>Laveena</h1>
-        <h1>Would you like to go for a Coffee?</h1>
+        <h1>L</h1>
+        <h1>W ffee?</h1>
       <div className='container'>
       <button type="button" className="btn btn-primary mx-2" onClick={yesbro} style={{fontSize:`${buttonSize}em`}}>Yes</button>
       <button type="button" className="btn btn-primary" onClick={nose} >{buttonText}</button>
